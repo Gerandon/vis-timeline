0cbf6a086e5486182bffe994140827f739cf897e
Gergo Asztalos <gergo0317@gmail.com>
2022-02-09 18:55:48 +0100

    remove generating examples circleci job

--- __snapshots__/package.test.js
+++ __snapshots__/package.test.js
@@ -1,5 +1,5 @@
 exports['Package Exported files 1'] = {
-  "name": "@gerandon/vis-timeline",
+  "name": "@gerandon/vis-timeline                  ",
   "files": [
     "        HISTORY.md",
     "        LICENSE.Apache-2.0.txt",
